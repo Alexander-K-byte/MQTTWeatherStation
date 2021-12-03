27bfbc49c8c789c8f2f51d1aa0f57e33dad2f833
M4nde <58217519+M4nde@users.noreply.github.com>
2021-12-03 18:03:02 +0200

    Add files via upload
    Weather station code for the Arduino working. No connection to MQTT yet.

---
+++ weather_station.h
@@ -0,0 +1,69 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-mqtt-publish-dht11-dht22-arduino/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include "DHT.h"
+#include <WiFi.h>
+extern "C" {
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+
+#define WIFI_SSID "Luuri"
+#define WIFI_PASSWORD "IoTessentials"
+
+// Digital pin connected to the DHT sensor
+#define DHTPIN 4  
+
+// Uncomment whatever DHT sensor type you're using
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)   
+
+// Initialize DHT sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+// Variables to hold sensor readings
+float temp;
+float hum;
+int i = 0;
+float totaltemp;
+float totalhum;
+float avgtemp;
+float avghum;
+
+
+void loop() {
+  unsigned long currentMillis = millis();
+  // Every X number of seconds (interval = 10 seconds) 
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    // New DHT sensor readings
+    hum = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    temp = dht.readTemperature();
+
+    //calculate the average for the publish
+    totaltemp += temp;
+    totalhum += hum; 
+    ++i;
+    avgtemp = totaltemp / i;
+    avghum = totalhum / i;
+    
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(temp) || isnan(hum)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+  }
+}
